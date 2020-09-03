c31fa4e568d2a79a6eb108a9273cf2e9c8087b53
Miske1996 <miskelvilaly@gmail.com>
2020-09-04 05:11:34 +0800

    Mise en place de l\'architecture du projet

---
+++ src/app/project/components/project-list/project-list.component.ts
@@ -0,0 +1,9 @@
+import { OnInit , Component } from '@angular/core';
+
+@Component({
+    selector: 'app-project-list',
+    templateUrl: './project-list.component.html'
+})
+export class ProjectListComponent implements OnInit {
+    ngOnInit(){}
+}
